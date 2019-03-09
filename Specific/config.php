26569ac73301749d819b2261a0c94bb6fb08b8ed
Tobias Brox <tobias@redpill-linpro.com>
2019-03-09 12:51:39 +0100

    The config and state needed for bringing up a baikal test-server ready for use by the caldav test suite (TODO: admin ui or admin password should probably be disabled)

---
+++ Specific/config.php
@@ -0,0 +1,7 @@
+<?php
+define("PROJECT_TIMEZONE", 'Europe/Paris');
+define("BAIKAL_CARD_ENABLED", TRUE);
+define("BAIKAL_CAL_ENABLED", TRUE);
+define("BAIKAL_DAV_AUTH_TYPE", 'Digest');
+define("BAIKAL_ADMIN_PASSWORDHASH", '210df7eec401f3bff7a142c82a5b4341')
+;
